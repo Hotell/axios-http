70a559adb6ed7e3ddb5fecb185351abf712fb1e6
Martin Hochel <hochelmartin@gmail.com>
2018-07-25 11:28:38 +0200

    chore(examples): update to latest version (#7)

--- examples/swapi-people-app/src/index.ts
+++ examples/swapi-people-app/src/index.ts
@@ -1,8 +1,18 @@
 import 'papercss/dist/paper.css'
 
+// tslint:disable-next-line:no-require-imports
+require('preact/devtools') // turn this on if you want to enable React DevTools!
+
 import { h, render } from 'preact'
+
 import { App } from './app'
 
 const mountTo = document.querySelector('main') as HTMLMainElement
 
-render(h<{}>(App, null), mountTo)
+let root: Element
+function boot() {
+  // let App = require('./components/app').default;
+  root = render(h<{}>(App, null), mountTo, root!)
+}
+
+boot()
